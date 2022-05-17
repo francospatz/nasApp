b1df9617ddb3740af541b40b09888bd63fee43b5
francospatz <francospatzdilorenzo@gmail.com>
2022-05-17 18:08:35 +0200

    Initial commit?

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(cors({origin: 'http://localhost:3000'}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
